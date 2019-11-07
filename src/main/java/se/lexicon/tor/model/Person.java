c20249b123082b4767c77c4ff655f9caa59be58d
Tor <arbiter99@live.com>
2019-11-07 12:44:29 +0100

    Added class Person, and tests for constructor and set/get tests

--- src/main/java/se/lexicon/tor/model/Person.java
+++ src/main/java/se/lexicon/tor/model/Person.java
@@ -1,1 +1,43 @@
+package se.lexicon.tor.model;
 
+
+public class Person
+{
+    private final int personId;
+    private String firstName, lastName;
+
+    //-----------------------------------------------------------------------------------------
+
+    public Person(int personId, String firstName, String lastName)
+    {
+        this.personId = personId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    //-----------------------------------------------------------------------------------------
+
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    //-----------------------------------------------------------------------------------------
+
+}
--- src/test/java/se/lexicon/tor/AppTest.java
+++ src/test/java/se/lexicon/tor/AppTest.java
@@ -1,20 +1,47 @@
 package se.lexicon.tor;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+import se.lexicon.tor.model.Person;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
+    private Person testPerson;
+
+    @Before
+    public void setup()
     {
-        assertTrue( true );
+        //arrange person
+        testPerson = new Person(241,"Tor","Lindström");
+    }
+
+    @Test
+    public void testConstructingPerson()
+    {
+        //assert
+        assertEquals(testPerson.getPersonId(), 241);
+        assertEquals(testPerson.getFirstName(), "Tor");
+        assertEquals(testPerson.getLastName(), "Lindström");
+    }
+
+    @Test
+    public void testSetFirstName()
+    {
+        //act
+        testPerson.setFirstName("Bengt");
+        //assert
+        assertEquals(testPerson.getFirstName(), "Bengt");
+    }
+
+    @Test
+    public void testSetLastName()
+    {
+        //act
+        testPerson.setLastName("Svensson");
+        //assert
+        assertEquals(testPerson.getLastName(), "Svensson");
     }
 }
